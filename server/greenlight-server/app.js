616aefdbfd0fbc8cd368f8b7d96a1e0e471ee656
plenhart <philip.lenhart@sport1.de>
2019-07-20 19:25:56 -0400

    add express server routes

--- server/greenlight-server/app.js
+++ server/greenlight-server/app.js
@@ -1,12 +1,78 @@
+var bodyParser = require('body-parser')
 import express from 'express'
 const app = express()
 
+const NodeCache = require( "node-cache" );
+const myCache = new NodeCache();
+
 import { employees } from "./mocks/employees"
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/employees', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(employees))
+const employeeList = employees
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/me/:id', function (req, res) {
+    const id = req.params.id
+    const me = employeeList[id]
+
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify(me))
 })
 
-app.listen(3001, () => console.log('Example app listening on port 3000!'))
+app.post('/me/:id/busy', function (req, res) {
+    const id = req.params.id
+    const me = employeeList[id]
+    me.time = req.body.time
+    employeeList[id] = me
+
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify(me))
+})
+
+app.post('/me/:id/available', function (req, res) {
+    const id = req.params.id
+    const me = employeeList[id]
+    me.time = null
+    employeeList[id] = me
+
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify(me))
+})
+
+app.get('/employees', function (req, res) {
+    res.setHeader('Content-Type', 'application/json')
+    const list = Object.keys(employeeList).map(function(key) {
+        return employeeList[key];
+    });
+    res.send(JSON.stringify(list))
+})
+
+app.post('/employees/:id/appoint', function (req, res) {
+    const id = req.params.id
+    const user = employeeList[id]
+    const me = employeeList[req.body.current_user]
+    user.queue = user.queue && user.queue.length > 0 ? user.queue.append(me) : [me]
+    employeeList[id] = user
+
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify(user))
+})
+
+app.post('/employees/:id/cancel-appoint', function (req, res) {
+    const id = req.params.id
+    const user = employeeList[id]
+    const me = employeeList[req.body.current_user]
+    user.queue = user.queue.filter((user) => {
+        return user.userName !== me.userName
+    })
+    employeeList[id] = user
+
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify(user))
+})
+
+app.listen(3001, () => console.log('Example app listening on port 3001!'))
